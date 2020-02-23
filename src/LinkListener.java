505c87899c278969819c3ce76bd9f659c9e4d167
Anna Alexandra Antonini <a.antonini011@studenti.unibs.it>
2020-02-23 14:17:31 +0100

    gestione links

---
+++ src/LinkListener.java
@@ -0,0 +1,29 @@
+import javax.swing.event.HyperlinkEvent;
+import javax.swing.event.HyperlinkListener;
+import java.awt.*;
+import java.io.IOException;
+import java.net.URISyntaxException;
+
+public class LinkListener implements HyperlinkListener
+    {
+    @Override
+    public void hyperlinkUpdate ( HyperlinkEvent hyperlinkEvent )
+        {
+        if(hyperlinkEvent.getEventType() == HyperlinkEvent.EventType.ACTIVATED) {
+        if ( Desktop.isDesktopSupported() && Desktop.getDesktop().isSupported(Desktop.Action.BROWSE)) {
+        try
+            {
+            Desktop.getDesktop().browse(hyperlinkEvent.getURL().toURI());
+            } catch ( IOException e )
+            {
+            e.printStackTrace();
+            } catch ( URISyntaxException e )
+            {
+            e.printStackTrace();
+            }
+        }
+
+        }
+
+        }
+    }
